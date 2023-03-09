dc739bad3c662f88d02bb50d865c51bc4a74afc5
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2023-03-09 11:09:21 -0500

    consume json api with httpClient

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(): void {
+    this.dataService.getPosts().subscribe((posts) => {
+      console.log(posts);
+    });
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
 }
